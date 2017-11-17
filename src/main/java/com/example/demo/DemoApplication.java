fa7a19f1c1f62e5f7847b5f99deb0f63a18ac13d
muyz.york <yingzhi.mu@live.cn>
2017-11-17 15:36:04 +0800

    mybatis新的配置方案，可自定义mapper xml文件位置，自定义datasource等

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,35 @@
 package com.example.demo;
 
+import com.example.demo.user.User;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootApplication
 public class DemoApplication {
 
+	@Autowired
+	private RedisTemplate redisTemplate;
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+
+		ApplicationContext ctx = SpringApplication.run(DemoApplication.class, args);
+
+//		RedisTemplate template = ctx.getBean(RedisTemplate.class);
+//		template.opsForValue().set("roles","001,002");
+	}
+	@Bean
+	public CommandLineRunner t1(){
+		return new CommandLineRunner() {
+			@Override
+			public void run(String... strings) throws Exception {
+				redisTemplate.opsForValue().set("roles","{\"code\",\"003\"}");
+				System.out.println(redisTemplate.opsForValue().get("roles"));
+			}
+		};
 	}
 }
--- src/main/java/com/example/demo/config/MyBatisConfig.java
+++ src/main/java/com/example/demo/config/MyBatisConfig.java
@@ -1,10 +1,69 @@
 package com.example.demo.config;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.transaction.annotation.TransactionManagementConfigurer;
+
+import javax.sql.DataSource;
+import java.sql.SQLException;
 
 /**
  * Created by muyz on 2017/11/7.
  */
+@Configuration
+@EnableTransactionManagement
 @MapperScan("com.example.demo.*.mapper")
-public class MyBatisConfig {
+public class MyBatisConfig implements TransactionManagementConfigurer {
+    /**
+     * mybatis 配置路径
+     */
+    private static String MYBATIS_CONFIG = "mybatis-config.xml";
+
+    @Autowired
+    private DataSource dataSource;
+//    @Bean
+//    public DataSource createDataSource() throws SQLException {
+//        return DataSourceBuilder.create(Thread.currentThread().getContextClassLoader())
+//                .driverClassName(jdbcConfig.getDriverClass())
+//                .url(jdbcConfig.url)
+//                .username(jdbcConfig.userName)
+//                .password(jdbcConfig.password).build();
+//    }
+    @Bean(name = "sqlSessionFactory")
+    public SqlSessionFactoryBean createSqlSessionFactoryBean() throws Exception {
+        SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+        PathMatchingResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
+
+        sqlSessionFactoryBean.setConfigLocation(new ClassPathResource(MYBATIS_CONFIG));
+
+        sqlSessionFactoryBean.setDataSource(dataSource);
+        sqlSessionFactoryBean.setMapperLocations(
+                resolver.getResources("classpath:mapper/**/*.xml"));
+        //sqlSessionFactoryBean.setTypeAliasesPackage("cn.emedchina.data.base");
+
+        return sqlSessionFactoryBean;
+    }
+
+
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+
+    @Bean
+    @Override
+    public PlatformTransactionManager annotationDrivenTransactionManager() {
+        return new DataSourceTransactionManager(dataSource);
+    }
 }
