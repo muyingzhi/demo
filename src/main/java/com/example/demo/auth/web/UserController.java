1f6545af7c18b00ff374e48ced8fd2822ab29137
muyz.york <yingzhi.mu@live.cn>
2017-11-20 14:09:11 +0800

    授权过程进行编码，未完待续。。。

--- src/main/java/com/example/demo/auth/web/UserController.java
+++ src/main/java/com/example/demo/auth/web/UserController.java
@@ -1,14 +1,14 @@
-package com.example.demo.user;
+package com.example.demo.auth.web;
 
+import com.example.demo.auth.User;
+import com.example.demo.auth.service.MyUserDetails;
+import com.example.demo.auth.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/user")
